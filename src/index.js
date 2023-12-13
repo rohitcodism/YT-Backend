2a9dbda1781ce40222433e7cd1d9a7c98cf538e1
rohitcodism <rohitpaula18i@gmail.com>
2023-12-13 23:44:12 +0530

    custom-api-response-check

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // require('dotenv').config({ path: './env' });
 import dotenv from "dotenv";
 import { connectDb } from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config(
     {
@@ -8,35 +9,16 @@ dotenv.config(
     }
 )
 
-connectDb();
-
-
-
-
-
-
-
-
-/*
-import express from "express";
-const app = express();
-
-;( async () => {
-    try 
-    {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("error", (error) => {
-            console.error( error );
-            throw error;
-        })
-
-        app.listen(process.env.PORT, () => {
-            console.log(`Server is running on port ${process.env.PORT}`);
-        });
-    }
-    catch(error)
-    {
-        console.error( error );
-    }
-} )();
-*/
+connectDb()
+.then((res) => {
+    app.on("error", (error) => {
+        console.log(`Server error: ${error}`);
+        throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port: ${process.env.PORT}`);
+    });
+})
+.catch((error) => {
+    console.log(`MongoDB connection ERROR: ${error}`);
+});
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+/*
+const asyncHandler = (fn) => async(req, res, next) => {
+    try {
+        await fn(req, res, next);
+    } catch (error) {
+        res.status(error.code || 500).json(
+            {
+                success: false,
+                message: error.message || "Internal server error",
+            }
+        )
+    }
+}
+*/
+
+const asyncHandler = (requestHandler) => {
+    (req, res, next) =>
+    {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+export {asyncHandler}
