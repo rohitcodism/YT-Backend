027e0d83d10839c5d2ae8416a586ef78e545d7bf
rohitcodism <rohitpaula18i@gmail.com>
2024-01-11 01:10:20 +0530

    ✅ Add deletePlaylist, getUserPlaylist, and getPlaylistById controller functions

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,4 +1,5 @@
 import { Playlist } from "../models/playlist.models.js";
+import { User } from "../models/user.models.js";
 import { Video } from "../models/video.models.js";
 import { apiError } from "../utils/apiError.js";
 import { apiResponse } from "../utils/apiResponse.js";
@@ -58,6 +59,90 @@ const createPlaylist = asyncHandler(async (req, res) => {
         )
 });
 
+const deletePlaylist = asyncHandler(async(req,res) => {
+    const {playlistId} = req.params;
+    const {incomingPassword} = req.body;
+
+    if(!playlistId || !incomingPassword){
+        throw new apiError("Did not got the playlist id!!!");
+    }
+
+    const user = await User.findById(req.user?._id);
+
+    if(!user){
+        throw new apiError(400, "Unauthorized request!!!");
+    }
+
+    const isValidPassword = user.isPasswordCorrect(incomingPassword);
+
+    if(!isValidPassword){
+        throw new apiError(400, "Invalid password!!!");
+    }
+
+    try {
+        await Playlist.findByIdAndDelete(playlistId);
+        console.log("Playlist deleted successfully");
+    } catch (error) {
+        console.error(error);
+        throw new apiError(500, "Something went wrong while deleting the playlist from the database!!!");
+    }
+
+    res
+    .status(200)
+    .json(
+        new apiResponse(
+            200,
+            null,
+            "Playlist deleted successfully."
+        )
+    )
+});
+
+const getUserPlaylist = asyncHandler(async(req,res) => {
+    const playlist = await Playlist.findOne({owner : req.user?._id});
+
+    if(!playlist){
+        throw new apiError(404, "Playlist not found!!!");
+    }
+
+    res
+    .status(200)
+    .json(
+        new apiResponse(
+            200,
+            playlist,
+            "Playlist fetched successfully."
+        )
+    )
+});
+
+const getPlaylistById = asyncHandler(async(req,res) => {
+    const {playlistId} = req.params;
+
+    if(!playlistId){
+        throw new apiError(400, "Did not get the playlist id!!!");
+    }
+
+    const playlist = await Playlist.findById(playlistId);
+
+    if(!playlist){
+        throw new apiError(400, "Cannot fetch playlist from database!!!");
+    }
+
+    res
+    .status(200)
+    .json(
+        new apiResponse(
+            200,
+            playlist,
+            "Playlist fetched successfully!!!",
+        )
+    )
+})
+
 export {
     createPlaylist,
+    deletePlaylist,
+    getUserPlaylist,
+    getPlaylistById
 }
--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,11 +1,17 @@
 import { Router } from "express";
-import { createPlaylist } from "../controllers/playlist.controller.js";
+import { createPlaylist, deletePlaylist, getPlaylistById, getUserPlaylist } from "../controllers/playlist.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 
 const router = Router();
 
 router.route("/create-playlist").post(verifyJWT,createPlaylist);
+
+router.route("/delete-playlist").delete(verifyJWT,deletePlaylist);
+
+router.route("/get-user-playlist").get(verifyJWT,getUserPlaylist);
+
+router.route("/get-playlist-by-id").get(verifyJWT,getPlaylistById);
 
 
 export default router;
